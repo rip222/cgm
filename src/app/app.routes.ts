6cb1780a1db3f8baa2d852d0ab913973e6bedae7
Ruslan K <ruslan@me>
2023-11-15 16:43:38 +0100

    feat: attach features to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { commitsResolver } from '@lib/feature/commits';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'repos',
+    loadComponent: () =>
+      import('@lib/feature/repos').then((c) => c.ReposComponent),
+  },
+  {
+    path: 'commits',
+    loadComponent: () =>
+      import('@lib/feature/commits').then((c) => c.CommitsComponent),
+    resolve: { commits: commitsResolver },
+  },
+  {
+    path: '**',
+    redirectTo: '/repos',
+    pathMatch: 'full',
+  },
+];
